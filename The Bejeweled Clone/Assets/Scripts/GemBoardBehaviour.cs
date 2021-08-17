5b8d31e8a8c1e665c2461004c441d0df48803f5d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-17 12:19:05 +0800

    Create matchesTracker in GemBoardBehaviour

--- The Bejeweled Clone/Assets/Scripts/GemBoardBehaviour.cs
+++ The Bejeweled Clone/Assets/Scripts/GemBoardBehaviour.cs
@@ -26,6 +26,7 @@ public class GemBoardBehaviour : MonoBehaviour
     public GemMovesAvailableChecker availableMovesChecker;
 
     public GameObject noMoreMovesDisplay;
+    public GemMatchesTracker matchesTracker;
 
     // Start is called before the first frame update
     void Start()
