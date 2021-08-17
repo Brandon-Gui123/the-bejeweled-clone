06dbf1bea124662149bc719a6ddb17663db58849
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-17 12:15:01 +0800

    Create matchesCounter field
    A TextMeshProUGUI component that displays the number of matches made.

--- The Bejeweled Clone/Assets/Scripts/GemMatchesTracker.cs
+++ The Bejeweled Clone/Assets/Scripts/GemMatchesTracker.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GemMatchesTracker : MonoBehaviour
 {
     int numMatchesMade = 0;
+    public TextMeshProUGUI matchesCounter;
 
     // Start is called before the first frame update
     void Start()
