6c8fd565cc6fa598c6226dde79c86211e212fc20
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-17 12:29:32 +0800

    Update board to Increment gems matched

--- The Bejeweled Clone/Assets/Scripts/GemBoardBehaviour.cs
+++ The Bejeweled Clone/Assets/Scripts/GemBoardBehaviour.cs
@@ -363,6 +363,15 @@ public class GemBoardBehaviour : MonoBehaviour
 
             if (hasMatchAvailable)
             {
+                // all gems in a match are to be added to a counter
+                foreach (var gem in gems)
+                {
+                    if (gem.hasBeenMatched)
+                    {
+                        matchesTracker.SetMatchCount(matchesTracker.numMatchesMade + 1);
+                    }
+                }
+
                 // shrink matched gems (animation)
                 yield return ShrinkMatchedGemsRoutine(isCascading);
 
