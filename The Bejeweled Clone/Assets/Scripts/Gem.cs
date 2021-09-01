b042f09573b1376d4666998028fca9e520a22b89
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-01 22:51:35 +0800

    Create hasBeenMatched property
    For compatibility with code that uses the 'hasBeenMatched' field on the
    GemBehaviour.

--- The Bejeweled Clone/Assets/Scripts/Gem.cs
+++ The Bejeweled Clone/Assets/Scripts/Gem.cs
@@ -9,6 +9,13 @@ public class Gem
         set => GemBehaviour.gemType = value;
     }
 
+    // TODO To be removed once all scripts have explicit access to hasBeenMatched on this Gem
+    public bool hasBeenMatched
+    {
+        get => GemBehaviour.hasBeenMatched;
+        set => GemBehaviour.hasBeenMatched = value;
+    }
+
     // TODO To be removed once all scripts have explicitly accessed the GemBehaviour via its property
     public static implicit operator GemBehaviour(Gem gem) => gem.GemBehaviour;
 }
