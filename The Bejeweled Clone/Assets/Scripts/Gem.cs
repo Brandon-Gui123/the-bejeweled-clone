6e853cfe9f8b9f00534ca5f3ebdae6fd283747ef
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-01 22:42:21 +0800

    Create gemType property
    For compatibility with GemBoardBehaviour, which eliminates the need to
    re-write code since the gemType can be accessed directly.
    
    Eventually, this property will be removed once all of the code has
    transitioned to use the new GemBoard class.

--- The Bejeweled Clone/Assets/Scripts/Gem.cs
+++ The Bejeweled Clone/Assets/Scripts/Gem.cs
@@ -2,6 +2,13 @@ public class Gem
 {
     public GemBehaviour GemBehaviour { get; set; }
 
+    // TODO To be removed once all scripts have explicitly accessed the GemType of the Gem
+    public GemTypes gemType
+    {
+        get => GemBehaviour.gemType;
+        set => GemBehaviour.gemType = value;
+    }
+
     // TODO To be removed once all scripts have explicitly accessed the GemBehaviour via its property
     public static implicit operator GemBehaviour(Gem gem) => gem.GemBehaviour;
 }
