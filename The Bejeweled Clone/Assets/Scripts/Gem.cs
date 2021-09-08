c721cc7ab1f2bc8dbbfafe1612682d78b8085f7f
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-08 13:55:43 +0800

    Remove old row/col properties from Gem class
    Those aren't needed since we have the newer RowOnBoard and ColOnBoard
    properties.

--- The Bejeweled Clone/Assets/Scripts/Gem.cs
+++ The Bejeweled Clone/Assets/Scripts/Gem.cs
@@ -20,20 +20,6 @@ public class Gem
         set => GemBehaviour.hasBeenMatched = value;
     }
 
-    // TODO To be removed once all scripts have explicit access to this
-    public int rowOnBoard
-    {
-        get => GemBehaviour.rowOnBoard;
-        set => GemBehaviour.rowOnBoard = value;
-    }
-
-    // TODO To be removed once all scripts have explicit access to this
-    public int colOnBoard
-    {
-        get => GemBehaviour.colOnBoard;
-        set => GemBehaviour.colOnBoard = value;
-    }
-
     // TODO To be removed once all scripts have explicitly accessed the GemBehaviour via its property
     public static implicit operator GemBehaviour(Gem gem) => gem.GemBehaviour;
 }
