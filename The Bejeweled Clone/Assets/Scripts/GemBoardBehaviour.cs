3ba02bb835c26d099e120fecc94c2e77426c5a33
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-08 20:29:45 +0800

    Update CheckForMatch to use HasBeenMatched

--- The Bejeweled Clone/Assets/Scripts/GemBoardBehaviour.cs
+++ The Bejeweled Clone/Assets/Scripts/GemBoardBehaviour.cs
@@ -214,7 +214,7 @@ public class GemBoardBehaviour : MonoBehaviour
         {
             foreach (var g in verticalGems)
             {
-                g.hasBeenMatched = true;
+                g.HasBeenMatched = true;
             }
         }
 
@@ -223,7 +223,7 @@ public class GemBoardBehaviour : MonoBehaviour
         {
             foreach (var g in horizontalGems)
             {
-                g.hasBeenMatched = true;
+                g.HasBeenMatched = true;
             }
         }
 
