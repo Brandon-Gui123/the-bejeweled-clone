12d0f4be749141f26e67dba49bbf8354c2a0beda
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-10 08:56:45 +0800

    Remove implicit cast from Gem to GemBehaviour

--- The Bejeweled Clone/Assets/Scripts/Gem.cs
+++ The Bejeweled Clone/Assets/Scripts/Gem.cs
@@ -7,7 +7,4 @@ public class Gem
     public bool IsEmpty { get; set; }
 
     public GemBehaviour GemBehaviour { get; set; }
-
-    // TODO To be removed once all scripts have explicitly accessed the GemBehaviour via its property
-    public static implicit operator GemBehaviour(Gem gem) => gem.GemBehaviour;
 }
